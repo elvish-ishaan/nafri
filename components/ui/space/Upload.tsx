68e4dff5bba29146b3a1fa921696ba5344f39bce
elvish-ishaan <ishaanelvish@gmail.com>
2025-02-17 20:51:35 +0530

    fixed unused imports

--- components/ui/space/Upload.tsx
+++ components/ui/space/Upload.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from '@/components/ui/tooltip';
 import { Plus } from 'lucide-react';
