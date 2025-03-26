603ace5707e862fe9e672b1f15eb7c411690e94c
elvish-ishaan <ishaanelvish@gmail.com>
2025-03-26 21:29:45 +0530

    removed unused files

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
     images: {
         domains: ["d3exjxhq0acftn.cloudfront.net","medrepo-bucket.s3.ap-south-1.amazonaws.com"],
       },
-      //this should be removed
-      eslint: {
-        ignoreDuringBuilds: true,
-      },
 };
 
 export default nextConfig;
